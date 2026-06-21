20c3c3a018a558f9645b8f9a419754fae2f16142
AMja26 <ahmadmuftiawwal1@gmail.com>
2026-06-21 22:52:25 +0700

    Add button controlled LED with debouncing

--- src/test.cpp
+++ src/test.cpp
@@ -1,16 +1,36 @@
 #include <Arduino.h>
 
+  int lastbuttonState = HIGH;
+  int phase = 0;
+  unsigned long lastpressTime;
+
 void setup() {
   pinMode(D1, OUTPUT);
+  pinMode(D2, OUTPUT);
+  pinMode(D3, INPUT);
 }
 
 void loop() {
-   for (int i = 0; i <= 255; i++) {
-    analogWrite(D1, i);
-    delay (5);
-   }
-   for (int i = 255; i >= 0; i--) {
-    analogWrite(D1, i);
-    delay (5);
-   }
+  bool buttonState = digitalRead(D3);
+
+  if (buttonState != lastbuttonState && millis() - lastpressTime > 50) {
+    if (buttonState == LOW) {
+      phase = (phase + 1)%3;
+      lastpressTime = millis();
+    }
+  }
+  lastbuttonState = buttonState;
+    
+  if (phase == 0){
+      digitalWrite(D1, LOW);
+      digitalWrite(D2, LOW);
+    }
+  else if (phase == 1){
+      digitalWrite(D1, HIGH);
+      digitalWrite(D2, LOW);
+    }
+  else if (phase == 2){
+      digitalWrite(D1, LOW);
+      digitalWrite(D2, HIGH);
+    }
 }
